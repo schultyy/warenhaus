3d3a975a8825226bad006c4f08f9aa819ffa1298
Jan Schulte <janschulte@fastmail.com>
2023-02-05 12:11:46 -0500

    send index requests to manager thread

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::convert::Infallible;
 use reqwest::StatusCode;
+use tokio::sync::mpsc::{self, Sender};
 use warp::Filter;
 use serde::{Serialize, Deserialize};
 use thiserror::Error;
-use tracing::{instrument, Level};
+use tracing::{instrument, error};
 use tracing::debug;
+
+fn with_tx(
+    tx: Sender<Command>,
+) -> impl Filter<Extract = (Sender<Command>,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || tx.clone())
+}
 
 #[derive(Debug, Deserialize)]
 pub enum Value {
@@ -37,21 +44,31 @@ impl IndexParams {
     }
 }
 
+#[derive(Debug)]
+enum Command {
+    Index(IndexParams)
+}
 
 #[tracing::instrument]
-async fn index_handler(index_params: IndexParams) -> Result<impl warp::Reply, Infallible> {
+async fn index_handler(tx: Sender<Command>, index_params: IndexParams) -> Result<impl warp::Reply, Infallible> {
     if let Err(err) = index_params.validate() {
         let json = warp::reply::json(&format!("{}", err));
         return Ok(warp::reply::with_status(json, StatusCode::UNPROCESSABLE_ENTITY))
     }
+
+    if let Err(err) = tx.send(Command::Index(index_params)).await {
+        error!("Error while trying to index data: {}", err);
+    }
+
     let json = warp::reply::json(&"OK");
     Ok(warp::reply::with_status(json, StatusCode::OK))
 }
 
-async fn web_handler() {
+async fn web_handler(tx: Sender<Command>) {
     let root = warp::path::end().map(|| "root");
 
     let index_data = warp::path!("index")
+        .and(with_tx(tx))
         .and(warp::post())
         .and(warp::body::json())
         .and_then(index_handler);
@@ -67,5 +84,16 @@ async fn web_handler() {
 async fn main() {
     tracing_subscriber::fmt::init();    
 
-    web_handler().await;
+    let (manager_tx, mut rx) = mpsc::channel(8192);
+    let web_tx = manager_tx.clone();
+    let mut all_workers = vec![];
+    let url_manager = tokio::spawn(async move {
+        while let Some(payload) = rx.recv().await {
+            println!("Received Index Payload: {:?}", payload);
+        }
+    });
+    all_workers.push(url_manager);
+
+    web_handler(web_tx).await;
+    futures::future::join_all(all_workers).await;
 }
